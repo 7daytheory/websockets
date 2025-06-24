c6bcf315ccf915445019a1ba657c8afe669a565f
Matthew Lowe <matthewjlowe11@gmail.com>
2025-06-24 14:07:54 -0400

    Setup websockets server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 //Websockets server
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8082});
+
+//ws refers to a single connection to the server side
+// If you have multiple connections at once, using many ws objects
+wss.on("connection", ws => {
+    console.log("New client Connected")
+
+    //Putting this on the ws single connection and not wss
+    ws.on("close", () => {
+        console.log("Client has disconnected")
+    })
+})
